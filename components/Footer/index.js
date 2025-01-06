5bb2bcc5dd9e8bf6e851e260a0143cbcda2b652b
zonnyzosa <bcwreford@gmail.com>
2025-01-06 21:43:05 +1100

    Update Text

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -27,10 +27,10 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{" "}
-        <Link href="https://zosa.co.uk/">
+        Made with ❤ by{" "} Ben :) 
+        {/* <Link href="https://zosa.co.uk">
           <a className="underline underline-offset-1">Zosa</a>
-        </Link>
+        </Link> */}
       </h1>
     </>
   );
